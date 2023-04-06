d552ea68d94fdcfaad87181666df6405ad99bead
Victor <v.henriquedasilva10@gmail.com>
2023-04-06 11:10:10 -0300

    Atualizando dados de redes sociais

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -3,13 +3,30 @@ import {BsLinkedin, BsGithub, BsInstagram} from 'react-icons/bs'
 
 const HeaderSocials = () => {
   return (
-    <div className='header__socials'>
-        <a href="http://linkedin.com" target="_blank" rel="noreferrer"><BsLinkedin/></a>
-        <a href="http://github.com" target="_blank" rel="noreferrer"><BsGithub/></a>
-        <a href="http://instagram.com" target="_blank" rel="noreferrer"><BsInstagram/></a>
-
+    <div className="header__socials">
+      <a
+        href="https://www.linkedin.com/in/victor-henrique-7952b4211/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <BsLinkedin />
+      </a>
+      <a
+        href="https://github.com/ViCtOrHenr1que"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <BsGithub />
+      </a>
+      <a
+        href="https://www.instagram.com/victor.h_18/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <BsInstagram />
+      </a>
     </div>
-  )
+  );
 }
 
 export default HeaderSocials
